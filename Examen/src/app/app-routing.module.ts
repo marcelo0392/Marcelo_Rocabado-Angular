4f02c3790e829dcff69fc9414a0829e4cd867cd8
Marcelo Rocabado <dragorr05@gmail.com>
2021-06-14 00:29:18 -0400

    examen

--- Examen/src/app/app-routing.module.ts
+++ Examen/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./landing-page/landing-page.module').then (m=>m.LandingPageModule)
+  },
+  {
+    path: 'error',
+    component: ErrorComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'error'
   }
 ];
 
